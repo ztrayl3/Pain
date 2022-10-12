ba5c64e5a965f22bf71208b8aff4cf9dd72d0b68
zacha <ztrayl3@lsu.edu>
2022-10-12 17:05:57 +0200

    Preliminary frequency analysis, not quite right. Suspect is the overlap and segment parameters, to be fixed next.

--- Freq.py
+++ Freq.py
@@ -1,46 +1,57 @@
 from mne.time_frequency import psd_welch
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas
 import pickle
+import mne
+mne.set_log_level(verbose="Warning")  # set all the mne verbose to warning
 
+stims = ['Stimulus/S  1', 'Stimulus/S  2', 'Stimulus/S  3']
 data = dict(male=None,
             female=None)
+bands = ["alpha", "beta", "gamma"]
+labels = [gender + " " + level for gender in data.keys() for level in stims]
+results = pandas.DataFrame(np.zeros((6, len(bands))), index=labels, columns=bands)  # to store the results cleanly
 
 # Load our epochs, male and female
 for gender in data.keys():
     source = open("epochs_{}.pkl".format(gender), "rb")
-    data[gender] = pickle.load(source)
+    data[gender] = pickle.load(source)  # load our epochs
     source.close()
 
-for gender in data.keys():
-    epochs = data[gender]
+    print("ANALYZING GENDER: {}".format(gender))
 
-    epochs.filter(l_freq=1.0, h_freq=None, n_jobs=-1)  # high-pass filter at 1Hz
-    epochs.notch_filter(50.0, n_jobs=-1)  # notch filter at 50Hz
+    for level in stims:
+        epochs = data[gender][level].copy()
+        epochs.filter(l_freq=1.0, h_freq=None, n_jobs=-1)  # high-pass filter at 1Hz
 
-    # Estimate PSDs based on average
-    kwargs = dict(fmin=1, fmax=100, n_jobs=-1,
-                  n_overlap=58, n_per_seg=63)
-    # "moving time window with a length of 250 ms and a step size of 20 ms"
-    # At 250Hz (sampling frequency), 250ms = 63 samples and 20ms = 5 samples
-    # With a step size of 5 samples, the number of points of overlap is 58 samples
-    psds_welch_mean, freqs_mean = psd_welch(epochs, average='mean', **kwargs, picks=['eeg'])
-    psds_welch_mean, freqs_mean = epochs.compute_psd('welch', average='mean', picks=['eeg'],
-                                                     tmin=.150, tmax=.350, **kwargs).get_data(return_freqs=True)
+        # "moving time window with a length of 250 ms and a step size of 20 ms"
+        # At 250Hz (sampling frequency), 250ms = 63 samples and 20ms = 5 samples
+        # With a step size of 5 samples, the number of points of overlap is 58 samples
+        psds_welch_mean, freqs_mean = psd_welch(epochs[level].average(), picks=["Cz", "FCz", "C2"],
+                                                fmin=1.0, fmax=100.0,
+                                                tmin=.150, tmax=.350,
+                                                n_overlap=20, n_per_seg=250)
 
-    # Convert power to dB scale.
-    psds_welch_mean = 10 * np.log10(psds_welch_mean)
+        # Convert power to dB scale.
+        psds_welch_mean = 10 * np.log10(psds_welch_mean)
 
-    # We will only plot the PSD for a single sensor in the first epoch.
-    ch_name = 'MEG 0122'
-    ch_idx = epochs.info['ch_names'].index(ch_name)
-    epo_idx = 0
+        alpha_range = np.arange(8.0, 12.5, 0.5)  # 8-12 Hz
+        alpha = []
+        beta_range = np.arange(12.5, 30.5, 0.5)  # 12-30Hz
+        beta = []
+        gamma_range = np.arange(30.5, 60.5, 0.5)  # 30-100Hz (limited to 60Hz for us)
+        gamma = []
+        powers = np.average(psds_welch_mean, axis=0)  # this averages across all channels and all subjects
+        for i in range(len(freqs_mean)):  # for each frequency...
+            if i in alpha_range:
+                alpha.append(powers[i])
+            elif i in beta_range:
+                beta.append(powers[i])
+            elif i in gamma_range:
+                gamma.append(powers[i])
 
-    _, ax = plt.subplots()
-    ax.plot(freqs_mean, psds_welch_mean[epo_idx, ch_idx, :], color='k',
-            ls='-', label='mean of segments')
-    ax.plot(freqs_median, psds_welch_median[epo_idx, ch_idx, :], color='k',
-            ls='--', label='median of segments')
-
-    ax.set(title=f'Welch PSD ({ch_name}, Epoch {epo_idx})',
-           xlabel='Frequency (Hz)', ylabel='Power Spectral Density (dB)')
-    ax.legend(loc='upper right')
+        label = gender + " " + level
+        results["alpha"][label] = np.mean(alpha)
+        results["beta"][label] = np.mean(beta)
+        results["gamma"][label] = np.mean(gamma)  # should be 228 +/-41
