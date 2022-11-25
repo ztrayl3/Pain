863c26ce6f0a7755b3fdf971a83310b146082edd
zatraylo <zatraylo@ncsu.edu>
2022-11-25 17:10:27 +0100

    Almost done with stats!

--- Stats/Analysis.R
+++ Stats/Analysis.R
@@ -1,4 +1,4 @@
-library(readxl)
+#### Imports and Libraries ####
 library(ggpubr)
 library(car)
 library(lme4)
@@ -13,28 +13,23 @@ library(ggplot2)
 require(gridExtra)
 library(ggsignif)
 
-# NOTE: EXCLUDE SUBJECT 18 FROM ALL ANALYSIS #
+
 
 #### ANOVA on Pain Threshold Data ####
 
 # Load Pain Threshold data
-threshold <- read_excel("Statistics.xlsx", 
-                         sheet = "Gender & Pain Threshold",
-                         col_types = c("numeric", "text", "numeric", 
-                                       "numeric", "skip", "skip", "skip", 
-                                       "skip", "skip", "skip", "skip", "skip", 
-                                       "skip", "skip", "skip"))
 
-# Assign variables as factors and rename them
-threshold$Sex <- as.factor(threshold$`Sex Name`)
-threshold$Threshold <- threshold$`Pain Threshold (mJ)`
-
-threshold <- subset(threshold, threshold$Subject != 33)  # remove single outlier
+threshold <- read_csv("Thresholds.csv", col_types = cols(Sex = col_factor(
+                                                       levels = c("male", "female"))))
 
 # Visualize distributions
 ggboxplot(threshold, x = "Sex", y = "Threshold", 
           color = "Sex", palette = c("#00AFBB", "#E7B800"),
           ylab = "Pain Threshold (mJ)", xlab = "Sex")
+ggqqplot(threshold, "Threshold", facet.by = "Sex")
+
+
+threshold <- subset(threshold, threshold$Subject != 33)  # remove single outlier
 ggqqplot(threshold, "Threshold", facet.by = "Sex")
 
 # Compute the analysis of variance
@@ -46,22 +41,27 @@ leveneTest(Threshold ~ Sex, data = threshold)
 aov_residuals <- residuals(object = thresh.aov)
 # Run Shapiro-Wilk test to check for normality (significant = non-normal)
 shapiro.test(x = aov_residuals)
+# Independence of observations: each subject is only male or female, so we're good
 
 # Summary of the analysis
 summary(thresh.aov)
 
 
+
 #### Within Subjects ANCOVA on Verbal Pain Rating Data ####
 
-ratings <- read_excel("Statistics.xlsx",
-                      sheet = "Gender & Pain Rating",
-                      col_types = c("numeric","text", "text", "text",
-                                    "skip", "numeric", "skip", "skip", "skip"))
+R1 <- read_csv("Perception_ratings.csv", col_types = cols(Sex = col_factor(
+                                                          levels = c("male", "female")),
+                                                          Stim = col_factor(
+                                                          levels = c("low", "med", "high"))))
+R2 <- read_csv("Control_ratings.csv", col_types = cols(Sex = col_factor(
+                                                       levels = c("male", "female")),
+                                                       Stim = col_factor(
+                                                       levels = c("low", "med", "high"))))
+ratings <- rbind(R1, R2)
+ratings <- ratings[sample(nrow(ratings), 5000), ]  # downsample, as we have too many ratings (>6000)
+ratings$Stim_Num <- as.numeric(ratings$Stim)  # useful for later
 
-# Rename and factor variables
-ratings$Sex <- as.factor(ratings$Sex)
-ratings$Pain <- as.numeric(ratings$`Verbal Pain Rating`)
-ratings$Stim <- as.factor(ratings$`Stimulus Level`)
 # omit NAs, where participant did not report within time limit
 ratings <- na.omit(ratings)
 
@@ -89,24 +89,27 @@ ggboxplot(ratings, x = "Stim", y = "Pain",
 pain.model <- lmer(Pain ~ Sex * Stim + (1|Subject), data = ratings, REML = TRUE)
 anova(pain.model)
 rand(pain.model)
+emm_options(pbkrtest.limit = 5000)
 post.hoc <- emmeans(pain.model, ~ Sex * Stim)
 pairs(post.hoc, adjust = "tukey")
 
-# Check the residuals for normality
+# Check the assumptions
 R = residuals(pain.model)
-qqnorm(R)
-qqline(R)
 ggqqplot(R)
 # Run Shapiro-Wilk test to check for normality (significant = non-normal)
 shapiro.test(R)
+# Levene's test for homogeneity of variance
+leveneTest(R ~ ratings$Sex * ratings$Stim)
+## NOTE: both of these tests are significant, but our sample size is big (Shapiro)
+## and there isn't a group imbalance (Levene), so this should be okay
 
-# a visualization of this significance when random effects are ignored
-ggboxplot(ratings, x = "Sex", y = "Pain", 
-          color = "Sex", palette = c("#00AFBB", "#E7B800"),
-          ylab = "Verbal Pain Rating", xlab = "Sex",
-          facet.by = "Stim") + 
-          geom_point(color = "grey") +
-          stat_summary(fun = mean, shape = 18)
+# Assumptions of homogeneity of regression slopes (should have no significant interaction)
+Anova(aov(Pain ~ Sex * Stim, data = ratings), type = 3)
+# Assumption of linearity (relationship between Pain and Stim at each Sex should be linear)
+ggplot(ratings, aes(Stim_Num, Pain, colour = Sex)) + geom_point(size = 3) + 
+  geom_smooth(method = "lm", aes(fill = Sex), alpha = 0.1) + theme(legend.position="top")
+
+
 
 #### ERP Component vs Baseline ####
 
