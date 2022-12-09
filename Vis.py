47c6b692d9241a700878857b13a115611a4b906e
zatraylo <zatraylo@ncsu.edu>
2022-12-09 15:18:26 +0100

    Vis now outputs good ERP figures and the data for R to make good Gamma figures. Analysis.rmd now makes those figures as well

--- Vis.py
+++ Vis.py
@@ -7,14 +7,16 @@ import mne
 def pretty_plot(EEG, title, ref, picks, ylim):
     epochs = EEG.copy()
     epochs.set_eeg_reference(ref_channels=ref)  # re-reference to average
-    evokeds = dict(low=epochs['Stimulus/S  1'].crop(tmin=0, tmax=0.5).average(),
-                   med=epochs['Stimulus/S  2'].crop(tmin=0, tmax=0.5).average(),
-                   high=epochs['Stimulus/S  3'].crop(tmin=0, tmax=0.5).average())
-    mne.viz.plot_compare_evokeds(evokeds, picks=picks,
-                                 title=gender.capitalize() + " " + title,
-                                 ylim=dict(eeg=ylim),
-                                 colors=colors[gender],
-                                 show=False)[0].savefig("Figures/" + gender + title + ".png")
+    evokeds = dict(low=epochs['Stimulus/S  1'].crop(tmin=-0.2, tmax=1).average(),
+                   med=epochs['Stimulus/S  2'].crop(tmin=-0.2, tmax=1).average(),
+                   high=epochs['Stimulus/S  3'].crop(tmin=-0.2, tmax=1).average())
+
+    mne.viz.plot_compare_evokeds(evokeds, picks=picks,  # choose only the channels we care about
+                                 title=gender.capitalize() + " " + title,  # title the graph
+                                 ylim=dict(eeg=ylim),  # scale y axis accordingly
+                                 colors=colors[gender],  # use custom color scheme
+                                 )[0].savefig("Figures/" + gender + title + ".png",
+                                              pad_inches=0, dpi=200)  # and save
 
 
 mne.set_log_level(verbose="Error")  # set all the mne verbose to warning
@@ -44,10 +46,12 @@ for gender in data.keys():
 
     # N1
     pretty_plot(epochs, "N1", ["Fz"], ["C4"], [-4, 4])
-    # N2
-    pretty_plot(epochs, "N2", "average", ["Cz"], [-5, 5])
+    # N2/P2 combined
+    pretty_plot(epochs, "N2 - P2", "average", ["Cz"], [-6, 6])
     # P2
-    pretty_plot(epochs, "P2", "average", ["Cz"], [-5, 5])
+    # pretty_plot(epochs, "P2", "average", ["Cz"], [-6, 6])
+
+quit()
 
 # Gamma Band Figures
 # Load our epochs, male and female
@@ -59,42 +63,41 @@ for gender in data.keys():
     data[gender] = pickle.load(s1)
     s1.close()
 
-    all_epochs.append(data[gender].copy().shift_time(-1, relative=True))
+    epochs = data[gender].copy().shift_time(-1, relative=True)
 
-epochs = mne.concatenate_epochs(all_epochs)
-epochs.filter(l_freq=1.0, h_freq=None)  # high-pass filter at 1Hz
+    epochs.filter(l_freq=1.0, h_freq=None)  # high-pass filter at 1Hz
 
-# "moving time window with a length of 250 ms and a step size of 20 ms"
-size = 0.250
-step = 0.020
-length = epochs.last / 1000  # the latest (in seconds) timepoint
-start = epochs.first / 1000  # the earliest (in seconds) timepoint
-stop = start + size
-timeseries = []
-axis = []
-
-while stop < length:
-    kwargs = dict(fmin=70, fmax=90,
-                  tmin=start, tmax=stop,
-                  picks=["Cz", "FCz", "C2"])
-    psds, freqs = epochs.compute_psd(**kwargs).get_data(return_freqs=True)
-
-    # Convert power to dB scale.
-    psds = 10 * np.log10(psds)
-
-    # average across epochs, channels, and frequency bands for a single max gamma amplitude value
-    gamma = np.mean(np.average(np.average(psds, axis=0), axis=0))
-    timeseries.append(gamma)
-
-    # keep track of time index and slide window
-    axis.append(np.round(start, 3))  # log a time point at the start of the window
-    start = start + step
+    # "moving time window with a length of 250 ms and a step size of 20 ms"
+    size = 0.250
+    step = 0.020
+    length = epochs.last / 1000  # the latest (in seconds) timepoint
+    start = epochs.first / 1000  # the earliest (in seconds) timepoint
     stop = start + size
+    timeseries = []
+    axis = []
 
-# "pre-stimulus baseline of -1000 to 0ms" (so 0 to +1 in our case)
-start_index, stop_index = axis.index(-1), axis.index(0)
-baseline = np.mean(timeseries[start_index:stop_index])
+    while stop < length:
+        kwargs = dict(fmin=70, fmax=90,
+                      tmin=start, tmax=stop,
+                      picks=["Cz", "FCz", "C2"])
+        psds, freqs = epochs.compute_psd(**kwargs).get_data(return_freqs=True)
 
-timeseries = np.array(timeseries) - baseline  # save baseline-corrected gamma power timseries
-pd.DataFrame({"Time": axis,
-              "Gamma": timeseries}).to_csv("Figures/GammaTS.csv")  # to a csv file for use in R
+        # Convert power to dB scale.
+        psds = 10 * np.log10(psds)
+
+        # average across epochs, channels, and frequency bands for a single max gamma amplitude value
+        gamma = np.mean(np.average(np.average(psds, axis=0), axis=0))
+        timeseries.append(gamma)
+
+        # keep track of time index and slide window
+        axis.append(np.round(start, 3))  # log a time point at the start of the window
+        start = start + step
+        stop = start + size
+
+    # "pre-stimulus baseline of -1000 to 0ms" (so 0 to +1 in our case)
+    start_index, stop_index = axis.index(-1), axis.index(0)
+    baseline = np.mean(timeseries[start_index:stop_index])
+
+    timeseries = np.array(timeseries) - baseline  # save baseline-corrected gamma power timseries
+    pd.DataFrame({"Time": axis,
+                  "Gamma": timeseries}).to_csv("Figures/{}GammaTS.csv".format(gender))  # to a csv file for use in R
